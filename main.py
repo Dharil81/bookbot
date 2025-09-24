d60123cb17800f73b13fc3c3e7ef33e3b06f731b
Dharil81 <rmacann@gmail.com>
2025-09-24 16:45:44 +1200

    add character counts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from stats import get_num_words, get_book_text, get_chars
+from stats import analyse_book
 
-get_chars("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")
-    
+analyse_book("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")
+#get_chars("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")    
 #get_num_words("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")
 #get_book_text("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -11,12 +11,31 @@ def get_num_words(filepath):
     print(f"Found {num_words} total words")   
 
 def get_chars(filepath):
-    get_num_words(filepath)
     lower = main(filepath).lower()
     chars = {}
     for char in lower:
-        if char in chars:
-            chars[char] += 1
-        else:
-            chars[char] = 1
-    print (dict(chars))
+        if char.isalpha():
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
+    return chars
+
+def sort_on(items):
+    return items["num"]
+
+def sort_chars(filepath):
+    output = get_chars(filepath)
+    char_counts = [{"char": char, "num": count} for char, count in output.items()]
+    char_counts.sort(reverse=True, key=sort_on)
+    return char_counts
+
+def analyse_book(filepath):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    get_num_words(filepath)
+    sorted = sort_chars(filepath)
+    for item in sorted:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
