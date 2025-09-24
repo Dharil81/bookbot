c114c5eeec4b8491bd79bd683736e19812822325
Dharil81 <rmacann@gmail.com>
2025-09-24 17:21:47 +1200

    make generic call to file

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import sys
 from stats import analyse_book
 
-analyse_book("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+filepath = sys.argv[1]
+
+analyse_book(filepath)
+
+#print(f"DEBUG: filepath is {filepath} and type is {type(filepath)}")
+
 #get_chars("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")    
 #get_num_words("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")
 #get_book_text("/Users/RJayne/workspace/github.com/dharil81/bookbot/books/frankenstein.txt")
